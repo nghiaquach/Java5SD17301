a79534ead4b49785f69115d3fec0812774d87bed
Nghia Quach <huunghiaquach@gmail.com>
2023-06-02 16:41:58 +0700

    Bai 6 - Report & DSL

---
+++ src/main/java/com/fpoly/controller/ProductController.java
@@ -0,0 +1,41 @@
+package com.fpoly.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.fpoly.dao.ProductDAO;
+import com.fpoly.model.Product;
+import com.fpoly.model.SearchForm;
+
+@Controller
+public class ProductController {
+
+    @Autowired
+    ProductDAO productDAO;
+
+    @GetMapping("/product/index")
+    public String index(Model model) {
+        var products = productDAO.findByCategoryById(1);
+        model.addAttribute("products", products);
+        model.addAttribute("searchForm", new SearchForm()); 
+
+        return "product/index";
+    }
+
+
+    @GetMapping("/product/search")
+    public String search(@RequestParam("searchText") String searchText, Model model) {
+
+        List<Product> searchResults = productDAO.findByNameLike("%"+searchText+"%");
+
+        model.addAttribute("searchResults", searchResults);
+        model.addAttribute("searchForm", new SearchForm()); 
+
+        return "product/index";
+    }
+}
--- src/main/java/com/fpoly/dao/ProductDAO.java
+++ src/main/java/com/fpoly/dao/ProductDAO.java
@@ -1,7 +1,16 @@
 package com.fpoly.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.fpoly.model.Product;
+import java.util.List;
 
-public interface ProductDAO extends JpaRepository<Product, Integer>{}
+
+public interface ProductDAO extends JpaRepository<Product, Integer>{
+    @Query("SELECT p FROM Product p WHERE p.category.Id=:id")
+    List<Product> findByCategoryById(@Param("id") Integer id);
+    
+    List<Product> findByNameLike(String name);
+}
